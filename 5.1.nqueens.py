a7d5a891c943bb078f15a19bf676304d4c6f492e
Jaehyun Byun <bjh1750@khu.ac.kr>
2024-12-14 03:01:58 +0900

    fix : 5.1

--- 5.1.nqueens.py
+++ 5.1.nqueens.py
@@ -1,18 +1,20 @@
 def promising(i, n, col):
-    for k in range(1, i):
-        if col[i] == col[k] or abs(col[i] - col[k]) == abs(i - k):
-            return False
-    return True
+    is_promising = True
+    k = 1
+    while k < i and is_promising:
+        if col[i] == col[k] or abs(col[i] - col[k]) == (i - k):
+            is_promising = False
+        k += 1
+    return is_promising
 
 def nqueens(i, n, col):
-    if i == n:
-        print("found=", col[1:])
-    else:
-        for c in range(1, n + 1):
-            col[i+1] = c
-            if promising(i+1, n, col):
+    if promising(i, n, col):
+        if i == n:
+            print("found=", col[1:])
+        else:
+            for j in range(1, n+1):
+                col[i+1] = j
                 nqueens(i+1, n, col)
-
 
 # Example 1
 print("######Example 1######")
@@ -21,7 +23,8 @@ col = [0] * (n + 1)
 nqueens(0, n, col)
 
 # Example 2 - Your Custom Case
-print("######Example 2 #######") 
-n = 5  
+print("######Example 2 #######")
+# Let's try with n = 5
+n = 5
 col = [0] * (n + 1)
 nqueens(0, n, col)
