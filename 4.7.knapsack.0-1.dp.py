5a8bed48ab8d1cfb6f5cadb11ccec87a3ce44b55
Jaehyun Byun <bjh1750@khu.ac.kr>
2024-12-13 21:23:00 +0900

    fix : 4.7 재귀말고 배열

--- 4.7.knapsack.0-1.dp.py
+++ 4.7.knapsack.0-1.dp.py
@@ -1,31 +1,27 @@
-def knapsack(n, W, DP):
-    global w, p
-    if n == 0 or W <= 0:
-        DP[(n, W)] = 0
-    else:
-        if w[n] > W:
-            DP[(n, W)] = knapsack(n - 1, W, DP) 
-        else:
-            DP[(n, W)] = max(knapsack(n - 1, W, DP), 
-                             knapsack(n - 1, W - w[n], DP) + p[n])
+def knapsack(n, W, w, p):
+    P = [[0]*(W+1) for _ in range(n+1)]
+    
+    for i in range(1, n+1):
+        for wt in range(W+1):
+            if w[i] <= wt:
+                P[i][wt] = max(P[i-1][wt], p[i] + P[i-1][wt - w[i]])
+            else:
+                P[i][wt] = P[i-1][wt]
 
-    return DP[(n, W)]
+    return P[n][W]
 
-# Example 1
+# 예제 1
 print("######Example 1######")
 n, W = 3, 30
-w = [0, 5, 10, 20]
-p = [0, 50, 60, 140]
-DP = {}
-maxprofit = knapsack(n, W, DP)
+w = [0, 5, 10, 20]  
+p = [0, 50, 60, 140] 
+maxprofit = knapsack(n, W, w, p)
 print("maxprofit: ", maxprofit)
 
-# Example 2 - Your Custom Case
-print("######Example 2 #######") 
-# Insert your example here
-n, W = 3, 50
-w = [0, 5, 10, 20]
-p = [0, 50, 60, 140]
-DP = {}
-maxprofit = knapsack(n, W, DP)
+# 예제 2 (사용자 정의 예제)
+print("######Example 2 #######")
+n, W = 4, 50
+w = [0, 10, 20, 30, 40]
+p = [0, 60, 100, 120, 240]
+maxprofit = knapsack(n, W, w, p)
 print("maxprofit: ", maxprofit)
