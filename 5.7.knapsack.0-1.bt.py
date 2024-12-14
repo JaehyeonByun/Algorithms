9045bf8d89f85e83c4c7911483b71044aa678382
Jaehyun Byun <bjh1750@khu.ac.kr>
2024-12-14 19:19:04 +0900

    Update 5.7.knapsack.0-1.bt.py

--- 5.7.knapsack.0-1.bt.py
+++ 5.7.knapsack.0-1.bt.py
@@ -1,15 +1,32 @@
 def promising(i, weight, profit):
     global n, W, w, p, maxprofit
-    # Complete the code here
+    if weight >= W:
+        return False
+    total_weight = weight
+    bound = profit
+    j = i + 1
+    while j <= n and total_weight + w[j] <= W:
+        total_weight += w[j]
+        bound += p[j]
+        j += 1
+    if j <= n:
+        bound += (W - total_weight) * (p[j] / w[j])
 
     return bound > maxprofit
-            
+
+
 def knapsack(i, weight, profit):
     global n, W, w, p, bestset, include, maxprofit
     if weight <= W and profit > maxprofit:
-        # Complete the code here
-    if promising(i, weight, profit):
-        # Complete the code here
+        maxprofit = profit
+        for k in range(1, n + 1):
+            bestset[k] = include[k]
+
+    if i < n and promising(i, weight, profit):
+        include[i + 1] = 1  
+        knapsack(i + 1, weight + w[i + 1], profit + p[i + 1])
+        include[i + 1] = 0 
+        knapsack(i + 1, weight, profit)
 
 # Example 1
 print("######Example 1######")
@@ -19,19 +36,17 @@ p = [0] + [40, 30, 50, 10]
 bestset, include = [0] * (n + 1), [0] * (n + 1)
 maxprofit = 0
 knapsack(0, 0, 0)
-print("maxprofit =",maxprofit)
-print("bestset =",bestset)
+print("maxprofit =", maxprofit)
+print("bestset =", bestset)
 
 
 # Example 2 - Your Custom Case
-print("######Example 2 #######") 
-# Insert your example here
-# n, W = 
-# w = [0] + 
-# p = [0] + 
+print("######Example 2 #######")
+n, W = 5, 15
+w = [0] + [1, 3, 4, 7, 8]
+p = [0] + [5, 6, 10, 12, 13]
 bestset, include = [0] * (n + 1), [0] * (n + 1)
 maxprofit = 0
-raise NotImplementedError("Complete your example.")
 knapsack(0, 0, 0)
-print("maxprofit =",maxprofit)
-print("bestset =",bestset)
+print("maxprofit =", maxprofit)
+print("bestset =", bestset)
